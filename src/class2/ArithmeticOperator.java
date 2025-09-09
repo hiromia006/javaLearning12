322fe83d32cf6a67657d01c234affaa1c149f9b0
hiromia006 <hiromia006@gmail.com>
2025-09-09 21:50:06 +0600

    Java Assignment two

---
+++ src/class2/ArithmeticOperator.java
@@ -0,0 +1,29 @@
+package class2;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        // Arithmetic Operators in Java
+        int a = 10;
+        int b = 5;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Sum: " + sum); //
+
+        // Subtraction
+        int difference = sum - b;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = sum * b;
+        System.out.println("Product: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulus
+        int remainder = a % b;
+        System.out.println("Remainder: " + remainder);
+    }
+}
